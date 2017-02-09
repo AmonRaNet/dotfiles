48844772711fe52ec66513e8dfc643ae23e4de18
AmonRaNet <amonra@ngs.ru>
2017-02-09 13:46:33 +0100

    i3: add display config & gtk settings

---
+++ config/i3/display.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+# xrandr --dpi 200
