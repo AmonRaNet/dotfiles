c38606cc34ff1aadecf20ca5e4c9fa2d63a46fe2
AmonRaNet <amonra@ngs.ru>
2017-04-23 23:06:00 +0200

    add fish functions: android-studio, gitkraken

---
+++ config/fish/functions/android-studio.fish
@@ -0,0 +1,4 @@
+function android-studio
+	nohup bash  /home/andrey/android-studio/bin/studio.sh >/dev/null 2>&1 &
+end
+
---
+++ config/fish/functions/gitkraken.fish
@@ -0,0 +1,4 @@
+function gitkraken
+	nohup gitkraken -p . >/dev/null 2>&1 &
+end
+
